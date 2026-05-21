a7a0b02302baedc8a58028f6e4f3d450bd3ba371
hawarya <aishwarya.v2023cse@sece.ac.in>
2026-05-21 12:35:43 +0530

    fix vercel routing

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: "https://dsa-tracker-mefa.vercel.app/"
+  origin: "https://dsa-tracker-mefa.vercel.app"
 }));
 app.use(express.json());
 
